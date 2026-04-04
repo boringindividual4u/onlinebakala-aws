b30058e2f1ecc07579b5024d7934a644545abd0e
boringindividual4u <rayzalllcus@gmail.com>
2026-04-04 19:03:11 +0000

    fix: update RDS endpoint to CMK encrypted instance v2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
     env: {
       NODE_ENV: 'production',
       PORT: 3000,
-      DB_HOST: 'netflix-production-db.c76msgoueoc9.eu-west-1.rds.amazonaws.com',      DB_NAME: 'netflix',
+      DB_HOST: 'netflix-production-db-v2.c76msgoueoc9.eu-west-1.rds.amazonaws.com',      DB_NAME: 'netflix',
       JWT_SECRET: 'bakala_super_secret_jwt_key_2026_change_this',
       COOKIE_SECRET: 'bakala_cookie_secret_2026'
     }
